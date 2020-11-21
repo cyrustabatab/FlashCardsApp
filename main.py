d7f2b255a198509574200411bc548fd449d1a79b
Cyrus Tabatabai-Yazdi <ctabatab@gmail.com>
2020-11-21 01:17:28 -0800

    added functionality so reset resets all cards

--- main.py
+++ main.py
@@ -1,28 +1,39 @@
 import tkinter,random,io,os
 from PIL import Image,ImageTk
 
-countries_to_capitals = {}
-countries = []
 
 progress_file = 'countries_to_learn.csv'
 original_file = 'countries_to_capitals.csv'
 seen_all_cards = False
-try:
-    with io.open(progress_file,'r',encoding='utf-8') as f:
-        for line in f:
-            country,capital = line.strip().split(',')
-            countries_to_capitals[country] = capital
-            countries.append(country) 
-except FileNotFoundError:
-    with io.open(original_file,'r',encoding='utf-8') as f:
-        for i,line in enumerate(f):
-            if i == 0:
-                continue
-            country,capital = line.strip().split(',')
-            countries_to_capitals[country] = capital
-            countries.append(country)
 
-random.shuffle(countries)
+
+def get_countries_and_mappings():
+    countries = []
+    countries_to_capitals = {}
+
+    try:
+        with io.open(progress_file,'r',encoding='utf-8') as f:
+            for line in f:
+                country,capital = line.strip().split(',')
+                countries_to_capitals[country] = capital
+                countries.append(country) 
+    except FileNotFoundError:
+        with io.open(original_file,'r',encoding='utf-8') as f:
+            for i,line in enumerate(f):
+                if i == 0:
+                    continue
+                country,capital = line.strip().split(',')
+                countries_to_capitals[country] = capital
+                countries.append(country)
+                if i == 4:
+                    break
+
+    random.shuffle(countries)
+    return countries,countries_to_capitals
+
+
+
+countries,countries_to_capitals = get_countries_and_mappings()
 countries_index = len(countries) - 1
 
 def switch_country():
@@ -34,6 +45,7 @@ def switch_country():
         seen_all_cards = True
         canvas.itemconfig(label,text='',fill='black')
         canvas.itemconfig(country_text,text="All Cards Seen!",fill='black')
+        canvas.itemconfig(card_image,image=front_image)
         if len(countries_to_capitals) > 0:
             with io.open(progress_file,'w',encoding='utf-8') as f:
                 for country,capital in countries_to_capitals.items():
@@ -67,16 +79,14 @@ def do_not_know_it():
         return
     switch_country()
 
-def reset():
-    pass
 BACKGROUND_COLOR = "#B1DDC6"
 
 showing_answer = False
 window = tkinter.Tk()
 window.configure(bg=BACKGROUND_COLOR,padx=50,pady=50)
 
-timer = window.after(3000,flip_card)
 
+timer = window.after(3000,flip_card)
 
 canvas = tkinter.Canvas(width=800,height=526,highlightthickness=0)
 canvas.configure(bg=BACKGROUND_COLOR)
@@ -99,13 +109,38 @@ right_image = tkinter.PhotoImage(file="images/right.png")
 right_button = tkinter.Button(image=right_image,highlightthickness=0,command=know_it)
 right_button.grid(row=1,column=1)
 
+def reset():
+    global timer
+    try:
+        window.after_cancel(timer)
+    except:
+        pass
+    global seen_all_cards,countries,countries_to_capitals,countries_index,current_country,showing_answer
+    showing_answer = False
+    countries,countries_to_capitals = get_countries_and_mappings()
+    random.shuffle(countries)
+    countries_index = len(countries) - 1
+    current_country = countries[countries_index]
+    canvas.itemconfig(label,text='Country')
+    canvas.itemconfig(country_text,text=current_country)
+    timer = window.after(3000,flip_card)
 
 reset_button = tkinter.Button(text="RESET",font=('Arial','40','normal'),command=reset)
 reset_button.grid(row=2,column=0,columnspan=2)
 
 
 
+
 window.mainloop()
+if not seen_all_cards:
+    # create write_to_file functiocn
+    with open(progress_file,'w',encoding='utf-8') as f:
+        for country,capital in countries_to_capitals.items():
+            f.write(country + ',' + capital + '\n')
+
+
+
+
 
 
 
